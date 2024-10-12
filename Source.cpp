4ece71bd640bc59d2f1e33b51f5683982bef5526
Audrius Pasvenskas <apasvenskas@outlook.com>
2024-10-12 16:57:24 -0700

    added some loging for testing the bounds and displaying floot and ceiling

--- Source.cpp
+++ Source.cpp
@@ -5,8 +5,15 @@ using namespace std;
 int nScreenWidth = 120;
 int nScreenHeight = 40;
 
-float fplayerX = 0.0f;
-float fplayerY = 0.0f; 
+float fplayerX = 8.0f;
+float fplayerY = 8.0f; 
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+float fFov = 3.13159/ 4.0; 
+float fDepth = 16; 
 
 int main(){
 
@@ -16,7 +23,68 @@ int main(){
 	SetConsoleActiveScreenBuffer(hConsole);
 	DWORD dwBytesWritten = 0;
 
+    // map
+    wstring map;
+
+    map += L"################";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"#..............#";
+    map += L"################";
+
     while(1){
+        // wall mechanics
+        for(int x = 0; x < nScreenWidth; x++){
+            float fRayAngle = (fPlayerA - fFov / 2.0f) + ((float)x / (float)nScreenWidth) * fFov;
+
+            float fDistanceToWall = 0;
+            bool bHitWall = false;
+
+            float fEyeX = sinf(fRayAngle);
+            float fEyeY = cosf(fRayAngle);
+
+            while(!bHitWall && fDistanceToWall < fDepth){
+                fDistanceToWall += 0.1f;
+
+                int nTestX = (int)(fplayerX + fEyeX * fDistanceToWall);
+                int nTestY = (int)(fplayerY + fEyeY * fDistanceToWall);
+
+                // test if ray is out of bounds
+                if(nTestX < 0|| nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight){
+                    bHitWall = true;
+                    fDistanceToWall = fDepth;
+                } else {
+                    if(map[nTestY * nMapWidth + nTestX] == '#'){
+                        bHitWall = true;
+                    }
+                }
+            }
+
+            // adjsuting the distance to ceiling and floor.
+            int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+            int nFloor = nScreenHeight - nCeiling;
+
+            for(int y = 0; y < nScreenHeight; y++){
+                if(y < nCeiling){
+                    screen[y*nScreenWidth + x] = ' ';
+                } else if(y > nCeiling && y <= nFloor) {
+                     screen[y*nScreenWidth + x] = '#';
+                } else {
+                    screen[y*nScreenWidth + x] = ' ';
+                }
+            }
+        }
 
         screen[nScreenWidth * nScreenHeight - 1] = '\0';
         WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
